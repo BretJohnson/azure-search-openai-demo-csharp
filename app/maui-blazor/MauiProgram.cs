f78a2ffde40134f18af7ea38a52cb011d75895b6
Matthew Leibowitz <mattleibow@live.com>
2023-11-02 01:09:03 +0200

    Update MauiProgram.cs

--- app/maui-blazor/MauiProgram.cs
+++ app/maui-blazor/MauiProgram.cs
@@ -22,6 +22,8 @@ public static class MauiProgram
         builder.Services.AddHttpClient<ApiClient>(client =>
         {
             // Configure this to point to your deployed API. For example, https://MY_HOSTED_APP.example.azurecontainerapps.io/
+            // If you want to use the Azure hosted web app, then the variables will automatically be set.
+            // If you are using a local web app, you will need to replace the URI here with your local host or dev tunnel.
             client.BaseAddress = new Uri(AppSettings.ServiceWebUri);
         });
         builder.Services.AddScoped<OpenAIPromptQueue>();
